670bae694d977236f1c6335edcf9570b406fdfb5
Mehdi Rizvi <mehdi.rizvi@gmail.com>
2020-08-08 16:30:17 +0500

    add snakes

---
+++ snakes/precache-manifest.123cefd11d407b9e01d9308a91507991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870a280abce14950a8ae3123203f4298",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c8af4ffa23296675ded",
+    "url": "/static/css/main.ced89492.chunk.css"
+  },
+  {
+    "revision": "c0f163c70306f3fed0de",
+    "url": "/static/js/2.ab86b07c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab86b07c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c8af4ffa23296675ded",
+    "url": "/static/js/main.ff21c1b7.chunk.js"
+  },
+  {
+    "revision": "38689e4d56806f8fd2ae",
+    "url": "/static/js/runtime-main.891d6e67.js"
+  }
+]);
